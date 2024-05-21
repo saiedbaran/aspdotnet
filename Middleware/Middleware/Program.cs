79c990d4e36eed03b32bd1bc5df9d8c26048c0d3
s.safikhani@outlook.com <saiedbaran>
2024-05-21 07:06:47 +0200

    simple middleware using USE method 

--- Middleware/Middleware/Program.cs
+++ Middleware/Middleware/Program.cs
@@ -1,6 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Use(async (HttpContext context, RequestDelegate next) =>
+    {
+        await context.Response.WriteAsync("First Hello \n");
+        await next(context);
+        await context.Response.WriteAsync("First Bye \n");
+    }
+);
+
+app.Use(async (HttpContext context, RequestDelegate next) =>
+    {
+        await context.Response.WriteAsync("Second Hello \n");
+        await next(context);
+        await context.Response.WriteAsync("Second Bye \n");
+    }
+);
+
+
+app.Run(async (HttpContext context)=>
+{
+    await context.Response.WriteAsync("Final Hello");
+});
 
 app.Run();
